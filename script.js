3e1daacc300b22159e6aff59bcd541b13ca5bd41
vibesbeCODING <vibesngb@gmail.com>
2024-11-22 13:20:03 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function popup(){
+    alert("welcome to Subconcious Studios")
+} 
+function message(){
+    alert("message sent")
+} 
+function exp(){
+    alert("EXPLORE YOUR DREAMS?")
+} 
+function imagine(){
+    alert("LET YOUR IMAGINATION GO WILD")
+} 
+function cd(){
+    alert("code sent")
+}
